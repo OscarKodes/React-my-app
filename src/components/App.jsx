a02bbddbfb43b5023a58456b9774283efc12bd30
Oscar Ko <OscarKoEmail@gmail.com>
2023-11-12 19:52:33 -0500

    Complete Keeper challenge part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+const noteCollection = notes.map((obj) => {
+  return <Note key={obj.key} title={obj.title} content={obj.content} />;
+});
 
 function App() {
   return (
     <div>
       <Header />
       <Note />
+      {noteCollection}
       <Footer />
     </div>
   );
